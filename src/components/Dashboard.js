f73e30327038cab837b6656fe4d1cf12539cba5a
edangx100 <edangx20@outlook.com>
2021-07-02 15:30:12 +0800

    test longer time span

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,62 +3,91 @@ import React, { useEffect, useState } from "react";
 import TempRow from "./TempRow"
 
 
-function determineDaysInMonth ( year, month ) {
-  const isLeapYear = (year % 4) || ((year % 100 === 0) &&
-      (year % 400)) ? 0 : 1;
-  const daysInMonth = (month === 2) ?
-      (28 + isLeapYear) : 31 - (month - 1) % 7 % 2;
+const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+const DAYS28 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
+const DAYS29 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"]
+const DAYS30 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" ]
+const DAYS31 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ]
+const HOURS = ["00","01","02"];
+// const HOURS = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
 
-  return daysInMonth;
+
+function determineDaysInMonth ( year, month ) {
+
+    const isLeapYear = (year % 4) || ((year % 100 === 0) &&
+        (year % 400)) ? 0 : 1;
+
+    const daysInMonth = (month === 2) ?
+        (28 + isLeapYear) : 31 - (month - 1) % 7 % 2;
+
+    return daysInMonth;
 }
 
+function selectDaysArray ( daysinMth_param ) {
+    let daysArrayToUse = [];
 
+    switch (daysinMth_param) {
+        case 28:
+            daysArrayToUse = [...DAYS28];
+            break;
+        case 29:
+            daysArrayToUse = [...DAYS29];
+            break;
+        case 30:
+            daysArrayToUse = [...DAYS30];
+            break;
+        case 31:
+            daysArrayToUse = [...DAYS31];
+            break;
+        default:
+            console.log( "Switch no match!");
+      }
+
+      return daysArrayToUse;
+}
+
+function sliceMonthSpan ( monthStart, monthEnd ) {
+    return MONTHS.slice( monthStart-1, monthEnd );
+}
+
+// ============================================================
 function Dashboard() {
 
     const [tempList, setTempList] = useState([]);
 
-    const stationID = "S117";
-        const days28 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
-        const days29 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"]
-        const days30 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" ]
-        const days31 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ]
-        const hours = ["00","01","02"];
-        // const hours = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
+    const stationID = "S24";   // Changi
         
-        const inputYear = 2020;
-        const inputMonth = 2;
-        const daysinMth = determineDaysInMonth( inputYear, inputMonth);
-        let daysArray = [];
+    const inputYear = 2020;
+    const inputMonthStart = 3;
+    const inputMonthEnd = 8;
+    
+    let monthsArray = [];
+    let daysArray = [];
+    
+    const monthSpan = sliceMonthSpan(inputMonthStart, inputMonthEnd);
+    const daysinMth = determineDaysInMonth( inputYear, inputMonthStart);
+    daysArray = selectDaysArray( daysinMth );
+    
+    console.log( daysinMth );
+    console.log( "daysArray: " + daysArray );
 
-        switch (daysinMth) {
-            case 28:
-                daysArray = [...days28];
-                break;
-            case 29:
-                daysArray = [...days29];
-                break;
-            case 30:
-                daysArray = [...days30];
-                break;
-            case 31:
-                daysArray = [...days31];
-                break;
-            default:
-                console.log( "Switch no match!");
-          }
 
 
     useEffect(() => {
 
         const fetchList = [];
-        for ( const hour of hours ) {
-          fetchList.push( fetch(`https://api.data.gov.sg/v1/environment/air-temperature?date_time=2020-01-01T${hour}%3A00%3A00`) );
-        }
 
-        for ( const day of daysArray )
-          for ( const hour of hours ) {
-              fetchList.push( fetch(`https://api.data.gov.sg/v1/environment/air-temperature?date_time=2020-01-${day}T${hour}%3A00%3A00`) );
-          }
+        // for ( const day of daysArray )
+        //   for ( const hour of HOURS ) {
+        //       fetchList.push( fetch(`https://api.data.gov.sg/v1/environment/air-temperature?date_time=2020-02-${day}T${hour}%3A00%3A00`) );
+        //   }
+
+        // Push by month
+        for ( const month of monthSpan )
+          for ( const day of daysArray )
+            for ( const hour of HOURS ) {
+                fetchList.push( fetch(`https://api.data.gov.sg/v1/environment/air-temperature?date_time=2020-${month}-${day}T${hour}%3A00%3A00`) );
+            }
 
         Promise.all(fetchList)
         .then((responses) => {
@@ -71,22 +100,32 @@ function Dashboard() {
         .then((data) => {
             console.log( data );
 
+            let missing = 0;
             for ( const object of data ) {
-              const temprArray = object.items[0].readings;
-              const temprObj = temprArray.find(element => element.station_id === stationID );
+              let temprArray;
+              let temprObj;
 
-              console.log( "time " + object.items[0].timestamp);
-              console.log( "temperature " + temprObj.value);
-              
-              setTempList( tempList =>[...tempList, <TempRow hour={object.items[0].timestamp} temperature={temprObj.value} />] ); 
+              try{
+                temprArray = object.items[0].readings;
+                temprObj = temprArray.find(element => element.station_id === stationID );
+
+                console.log( "time " + object.items[0].timestamp);
+                console.log( "temperature " + temprObj.value);
+
+                setTempList( tempList =>[...tempList, <TempRow hour={object.items[0].timestamp} temperature={temprObj.value} />] ); 
+              }
+              catch {
+                console.log( "Missing object");
+                missing += 1;
+              }
             }
+            console.log("Missing: " + missing );
         })
         .catch((error) => {
             console.log(error);
         });
 
-        // console.log( "tempList Length? " + tempList.length );
-
+        console.log( "tempList Length? " + tempList.length );
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, []);
 
--- src/testLeap.js
+++ src/testLeap.js
@@ -1,4 +1,13 @@
 
+const MONTHS = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+const DAYS28 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
+const DAYS29 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"]
+const DAYS30 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" ]
+const DAYS31 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ]
+const HOURS = ["00","01","02"];
+// const HOURS = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
+
+
 function determineDaysInMonth ( year, month ) {
 
     const isLeapYear = (year % 4) || ((year % 100 === 0) &&
@@ -10,43 +19,66 @@ function determineDaysInMonth ( year, month ) {
     return daysInMonth;
 }
 
+function selectDaysArray ( daysinMth_param ) {
+    let daysArrayToUse = [];
 
-const days28 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28"]
-const days29 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29"]
-const days30 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30" ]
-const days31 = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" ]
-const hours = ["00","01","02"];
+    switch (daysinMth_param) {
+        case 28:
+            daysArrayToUse = [...DAYS28];
+            break;
+        case 29:
+            daysArrayToUse = [...DAYS29];
+            break;
+        case 30:
+            daysArrayToUse = [...DAYS30];
+            break;
+        case 31:
+            daysArrayToUse = [...DAYS31];
+            break;
+        default:
+            console.log( "Switch no match!");
+      }
+
+      return daysArrayToUse;
+}
+
+function sliceMonthSpan ( monthStart, monthEnd ) {
+    return MONTHS.slice( monthStart-1, monthEnd );
+}
+
+
+// ==================================================================
 
 const inputYear = 2020;
-const inputMonth = 12;
-const daysinMth = determineDaysInMonth( inputYear, inputMonth);
+const inputMonthStart = 2;
+const inputMonthEnd = 8;
+
+let monthsArray = [];
 let daysArray = [];
 
-switch (daysinMth) {
-    case 28:
-        daysArray = [...days28];
-        break;
-    case 29:
-        daysArray = [...days29];
-        break;
-    case 30:
-        daysArray = [...days30];
-        break;
-    case 31:
-        daysArray = [...days31];
-        break;
-    default:
-        console.log( "Switch no match!");
-  }
+const monthSpan = sliceMonthSpan(inputMonthStart, inputMonthEnd);
+const daysinMth = determineDaysInMonth( inputYear, inputMonthStart);
+daysArray = selectDaysArray( daysinMth );
 
 console.log( daysinMth );
 console.log( "daysArray: " + daysArray );
 
 
 const fetchList = [];
-for ( const day of daysArray )
-    for ( const hour of hours ) {
-        fetchList.push( `API?date_time=2020-01-${day}T${hour}%3A00%3A00` );
-    }
+
+// Push by month
+for ( const month of monthSpan )
+    for ( const day of daysArray )
+        for ( const hour of HOURS ) {
+            // fetchList.push( `API?date_time=2020-${month}-${day}T${hour}%3A00%3A00` );
+            fetchList.push( `Month ${month} - Day ${day} Hour ${hour}` );
+        }
 
 console.log( fetchList );
+console.log( fetchList.length );
+
+
+
+
+
+https://api.data.gov.sg/v1/environment/air-temperature?date_time=2020-03-23T00%3A00%3A00
